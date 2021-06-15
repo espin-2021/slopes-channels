b7276e15652a3e2f4b0759d195f34bf99b5e17c7
jkpierce <kevinkayaks@gmail.com>
2021-06-15 07:19:39 -0700

    reorganized space stuff slightly.

---
+++ readme.md
@@ -0,0 +1,2 @@
+# slopes-channels
+evaluating the effects of hillslope sediment supply on basin outlet sediment fluxes 
--- space-exploring/SPACE_example.py
+++ space-exploring/SPACE_example.py
@@ -14,95 +14,130 @@ Created on Tue Jun  1 13:35:03 2021
 """
 #%% IMPORT
 import numpy as np
+import matplotlib.pyplot as plt
+
 from landlab import RasterModelGrid, imshow_grid
 from landlab.components import (FlowAccumulator,
                                 DepressionFinderAndRouter,
                                 Space,
-                                FastscapeEroder)
+                                LinearDiffuser)
 import time
 
+#%% Set up parameters for model (units are m and yr)
+
+dt = 50
+runtime = 100000
+
+uplift_rate = 0.0001
+K_dif = 0.005
+
+soil_depth_initial = 0.0
+K_sed=0.0005
+K_br=0.0001
+F_f=0
+phi=0
+H_star=1.0
+v_s=2.0
+m_sp=0.5
+n_sp = 1.0
+sp_crit_sed=0
+sp_crit_br=0
+solver='basic'
+
 #%% Set up raster model grid
+
+num_rows = 30
+num_cols = 30
+dx = 100.
+
 np.random.seed(seed = 5000)
 
-mg = RasterModelGrid((20, 20), xy_spacing=100.0)
+mg = RasterModelGrid((num_rows, num_cols), xy_spacing=dx)
 _ = mg.add_zeros('topographic__elevation', at='node')
-mg.at_node['topographic__elevation'] += (mg.node_y / 10000. +
-                                         mg.node_x / 10000. +
-                                         np.random.rand(len(mg.node_y)) / 10.)
+
+# Random mm-scale noise
+mg.at_node['topographic__elevation'] += (np.random.rand(len(mg.node_y)) / 100.)
+
+# Sloped surface
+mg.at_node['topographic__elevation'] += (mg.node_y / 100000. +
+                                         mg.node_x / 100000.)
+
+# Boundary conditions
 mg.set_closed_boundaries_at_grid_edges(bottom_is_closed=True,
                                        left_is_closed=True,
                                        right_is_closed=True,
                                        top_is_closed=True)
 mg.set_watershed_boundary_condition_outlet_id(
     0, mg.at_node['topographic__elevation'], -9999.)
-fsc_dt = 100.
-space_dt = 100.
 
-#%% Visualize grid
-
-imshow_grid(mg, 'topographic__elevation')
-
-#%% Instantiate Fastscape eroder, flow router, and depression finder
-
-fr = FlowAccumulator(mg, flow_director='D8')
-df = DepressionFinderAndRouter(mg)
-fsc = FastscapeEroder(mg, K_sp=.001, m_sp=.5, n_sp=1)
-
-#%% Burn in an initial drainage network using the Fastscape eroder:
-
-for x in range(100):
-    fr.run_one_step()
-    df.map_depressions()
-    fsc.run_one_step(dt=fsc_dt)
-    mg.at_node['topographic__elevation'][0] -= 0.001 # Uplift
-
-#%% Visualize grid
-
-imshow_grid(mg, 'topographic__elevation')
-
-#%% Add some soil to the drainage network:
-
+# Soil depth
 _ = mg.add_zeros('soil__depth', at='node', dtype=float)
-mg.at_node['soil__depth'] += 2.0
+mg.at_node['soil__depth'] += soil_depth_initial
 mg.at_node['topographic__elevation'] += mg.at_node['soil__depth']
 
 #%% Visualize grid
-
+fig = plt.figure()
+plot = plt.subplot()
 imshow_grid(mg, 'topographic__elevation')
+plt.show()
 
-#%% Instantiate the Space component:
+#%% Instantiate components:
 
-ha = Space(mg,
-           K_sed=0.00001,
-           K_br=0.00000000001,
-           F_f=0.5,
-           phi=0.1,
-           H_star=1.,
-           v_s=5.0,
-           m_sp=0.5,
-           n_sp = 1.0,
-           sp_crit_sed=0,
-           sp_crit_br=0,
-           solver='basic')
+fr = FlowAccumulator(mg, flow_director='D8')
+df = DepressionFinderAndRouter(mg)
+ld = LinearDiffuser(mg, linear_diffusivity=K_dif)
+sp = Space(mg,
+           K_sed=K_sed,
+           K_br=K_br,
+           F_f=F_f,
+           phi=phi,
+           H_star=H_star,
+           v_s=v_s,
+           m_sp=m_sp,
+           n_sp=n_sp,
+           sp_crit_sed=sp_crit_sed,
+           sp_crit_br=sp_crit_br,
+           solver=solver)
 
-#%% Now run the Space component for 2000 short timesteps:
+#%% Run model time loop:
 
 start_time = time.time()
 
-for x in range(2000):
+for x in range(runtime//dt):
     fr.run_one_step()
     df.map_depressions()
-    ha.run_one_step(dt=space_dt)
-    mg.at_node['bedrock__elevation'][0] -= 2e-6 * space_dt
-    if x % 100 == 0:
-        print("--", x*space_dt, "years -- %s seconds --" % round((time.time() - start_time), 1))
+    sp.run_one_step(dt=dt)
+    ld.run_one_step(dt=dt)
+    mg.at_node['bedrock__elevation'][mg.core_nodes] += uplift_rate * dt
+    if x*dt % 1000 == 0:
+        print("--", x*dt, "years -- %s seconds --" % round((time.time() - start_time), 1))
 
-#%% Visualize grid
+#%% PLOT: topo map and sediment flux map
 
-imshow_grid(mg, 'topographic__elevation')
+#Show topo map
+fig = plt.figure()
+plot = plt.subplot()
+imshow_grid(mg, 'topographic__elevation', plot_name='Topographic Elevation', var_name = 'Topographic Elevation', var_units='m', grid_units=('m', 'm'), cmap='terrain')
+plt.show()
 
-#%%
-#%%
+#Show sediment flux map
+fig = plt.figure()
+plot = plt.subplot()
+imshow_grid(mg, 'sediment__flux', plot_name='Sediment flux', var_name = 'Sediment flux', var_units=r'm$^3$/yr', grid_units=('m', 'm'), cmap='Blues')
+plt.show()
+
+#%% PLOT: slope-area
+
+area = mg.at_node['drainage_area'][mg.core_nodes]
+slope = mg.at_node['topographic__steepest_slope'][mg.core_nodes]
+
+fig = plt.figure()
+slope_area_plot = plt.subplot()
+slope_area_plot.set_xscale('log')
+slope_area_plot.set_yscale('log')
+plt.scatter(area, slope)
+plt.show()
+
 #%%
 """
     References
@@ -115,34 +150,3 @@ imshow_grid(mg, 'topographic__elevation')
     https://dx.doi.org/10.5194/gmd-10-4577-2017
 
  """
-
-#%%
-"""Run step with CHILD-like solver that adjusts time steps to prevent slope
- flattening."""
-
-from landlab import RasterModelGrid
-from landlab.components import FlowAccumulator
-import numpy as np
-
-rg = RasterModelGrid((3, 4))
-z = rg.add_zeros('topographic__elevation', at='node')
-z[:] = 0.1 * rg.x_of_node
-H = rg.add_zeros('soil__depth', at='node')
-H += 0.1
-br = rg.add_zeros('bedrock__elevation', at='node')
-br[:] = z - H
-
-fa = FlowAccumulator(rg, flow_director='FlowDirectorSteepest')
-fa.run_one_step()
-sp = Space(rg, K_sed=1.0, K_br=0.1,
-           F_f=0.5, phi=0.0, H_star=1., v_s=1.0,
-           m_sp=0.5, n_sp = 1.0, sp_crit_sed=0,
-           sp_crit_br=0, solver='adaptive')
-sp.run_one_step(dt=10.0)
-
-#np.round(sp.Es[5:7], 4)
-#array([ 0.0029,  0.0074])
-#np.round(sp.Er[5:7], 4)
-#array([ 0.0032,  0.0085])
-#np.round(H[5:7], 3)
-#array([ 0.088,  0.078])
